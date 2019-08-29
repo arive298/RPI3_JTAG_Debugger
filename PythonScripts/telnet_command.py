79d6fc8e53e702af0b19bb9aa7d6a66313f2a552
Alex Rivero <arive529@gmail.com>
2019-08-29 12:32:12 -0400

    Added comments for better understanding

--- PythonScripts/telnet_command.py
+++ PythonScripts/telnet_command.py
@@ -4,8 +4,15 @@ HOST = "localhost"
 tn = telnetlib.Telnet(HOST,4444)
 DEBUG_TELNET = False
 DEBUG = False
-# use to send a singe command withut paramteres
+
 def send_telnet_command(cmd, param='', speed=2):
+    """
+    Used to send a telnet command to openocd
+    @ Param: cmd - Command to send
+    @ Param: param - Parameter for the openocd
+    @ Param: speed - To change time-out of the read function 
+    @ Return: 
+    """
     if(DEBUG_TELNET):
        print('writing to telnet server')
     cmd = cmd+' '+param+"\r\n"
@@ -18,25 +25,44 @@ def send_telnet_command(cmd, param='', speed=2):
     ret = tn.read_until(b'jsfjjsd',speed)
     return ret.decode('utf-8')
 
-#Stripp address from the obtained bytes of the telnet server    
-def get_stripped_reg(reg):
-    index = reg.find('0x')
-    reg = reg[index:(index+10)]
-    return reg
+def get_stripped_reg(reg_contents):
+    """
+    Strip the obtained address from the register after getting string the telnet command "reg <reg # or name>     
+    @ Param: reg_contents - The contents of the register to strip
+    @ Returns: the string address
+    """
+    index = reg_contents.find('0x')
+    addr = reg_contents[index:(index+10)]
+    return addr
 
 def get_stripped_address(address):
+    """
+    Function used to strip a specific address from the returned telnet command 
+    @ Param: adress - The starting address to strip the name
+    @ Returns: the string address
+    """
     index = address.find(':')
     index = index + 2
     address = address[index:(index+8)]
     return address
 
 def get_stripped_name(address):
+    """
+    Function used to get the stripped address of the task from the 'mdw' telnet command
+    @ Param: address- The starting address to strip the name
+    @ Returns: The hex string 
+    """
     index = address.find(':')
     index = index + 2
     name_in_hex = (address[index:]).replace(" ","").replace("00000000","").replace("\r","").replace("\n","").replace(">","")
     return name_in_hex
 
 def change_endian(address):
+    """
+    Used to change the little endian format of string in RPI3 to readable format. (Note: it works only for dword)
+    @ Param - address The address to change endianes
+    @ Returns a hex list that can be later converted
+    """
     result = []
     for i in range(int(len(address)/8)):  
        start = 7+(i*8)
@@ -46,23 +72,39 @@ def change_endian(address):
     for i in range(len(result)):
         result[i] = int(result[i],16)
         
-    string_result = ''.join(chr(i) for i in result)
-    return string_result
+    return result
 
 def get_task_name(task_addr):
-    task_addr = hex( int(task_addr,16) + int('0x4ac',16) )
+    """
+    Function to get the decoded string name of a task from a given task address
+    @ Param - task_addr Address of task for which the name will be extracted
+    @ Returns the fully converted string
+    """
+    task_addr = hex( int(task_addr,16) + int('0x4ac',16) ) 
     contents = get_stripped_name(send_telnet_command('mdw', (task_addr + " 4"), 0.1))
     endians = change_endian(contents)
-    return endians
+    string_result = ''.join(chr(i) for i in endians)
+    return string_result
 
-def get_first_task(address):
-    bin_addr = hex( ((int(address,16)) & int('0xffffe000',16) ) + int('0xc',16)) 
+def get_first_task(sp_contents):
+    """
+    This function is only used for obtaining the first task(currently running task) because it requires some specific calculations involving the stack pointer, 
+    and the same math does not apply to the other tasks.
+    @ param - sp_contents The address obtained from sp 
+    @ Returns the address of the first task(currently running task)
+    """
+    bin_addr = hex( ((int(sp_contents,16)) & int('0xffffe000',16) ) + int('0xc',16)) 
     task = get_stripped_address( send_telnet_command('mdw', bin_addr, 0.1))
     task = hex(int(task,16))
     print(task)
     return task
 
 def get_next_task(address):
+    """
+    Gets the address in memory of the next task
+    @ Param - address of the current task
+    @ Returns the calculated address
+    """
     next_pointer = hex( (int(address,16))  + int('0x300',16))
     next_address = get_stripped_address( send_telnet_command('mdw', next_pointer, 0.1))
     next_task = hex( (int(next_address,16))  - int('0x300',16))
@@ -77,25 +119,48 @@ def get_next_task(address):
     return next_task
 
 def get_previous_task(address):
+    """
+    Gets the address in memory of the previous task
+    @ Param - address of the current task
+    @ Returns the calculated address
+    """
     next_pointer = hex( (int(address,16))  + int('0x304',16))
     next_address = get_stripped_address( send_telnet_command('mdw', next_pointer, 0.1))
     next_task = hex( (int(next_address,16))  - int('0x300',16))
     return next_task
 
-def find_tasks(task_name):
-    print(send_telnet_command("halt"))
+def find_tasks(task_name, loop_backward = True):
+    """
+    This function finds the address of a specific task. Thu function loop through the process list to find a matching name.
+    you can ither loop foreward or backward by passing false as the second parameter
+    @ Param - task_name Name of the task to find
+    @ Param - loop_backward Looping direction, defaulted to backward
+    @ Returns the found address, other wise it returns 'not found'
+    """
+    halt_result = send_telnet_command("halt")
+    
+    if(DEBUG):
+        print("Task to find: "+task_name)
+        print(halt_result)
+
     sp = get_stripped_reg(send_telnet_command("reg","90",0.1))
     addr = get_stripped_address(send_telnet_command("mdw",sp,0.1))
     current_address = get_first_task(addr)
     
     current_name = get_task_name(current_address)
     first_proc_name = current_name
+    
     if(DEBUG):
         print("First process name: "+first_proc_name)
     
     count = 0
     while (current_name != task_name and (task_name not in current_name) ):
-        current_address = get_next_task(current_address)
+       
+        if(loop_backward):
+            current_address = get_previous_task(current_address)
+        else:
+            current_address = get_next_task(current_address)
+
         current_name = get_task_name(current_address)
        
         if(DEBUG):
